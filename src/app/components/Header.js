6df928917215d42f633e5dc0ab2c7cd2c5f4bb02
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2024-06-09 18:43:24 -0300

    Feat: Add web3js lib, and make metamask authentication

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,4 +1,22 @@
+'use client'
+import { useEffect, useState } from "react"
+import { doLogin } from "../services/Web3Service"
+
 export default function Header () {
+
+  const [wallet, setWallet] = useState("");
+
+  useEffect(() => {
+    setWallet(localStorage.getItem("wallet") || "");
+  },[])
+
+  const btnLoginClick = ()  => {
+    doLogin().then(wallet => setWallet(wallet)).catch(err => {
+      console.error(err);
+      alert(err.message);
+    })
+  }
+
   return(
     <div className="mx-auto py-5 bg-slate-900 flex items-center justify-center">
       <header className="p-3 bg-slate-900 flex flex-1 flex-row justify-between items-center max-w-[1200px]">
@@ -8,11 +26,16 @@ export default function Header () {
           </a>
         </div>
         <div className="flex flex-row items-center space-x-8 m-0">
-          <div className="flex flex-row items-center">
-            <img src="/metamask.svg" className="w-6"/>
-          <button type="button" className="flex border-solid border border-white text-slate-100 p-2 rounded-md"> Entrar</button>
-          </div>
-          <button type="button" className="bg-yellow-400 text-slate-900 p-2 rounded-md"><a href="/create">Pedir Ajuda</a></button>
+          {
+            wallet 
+              ?  <button type="button" className="bg-yellow-400 text-slate-900 p-2 rounded-md"><a href="/create">Pedir Ajuda</a></button>
+              :  (
+                <div className="flex flex-row items-center">
+                  <img src="/metamask.svg" className="w-6"/>
+                  <button type="button" className="flex border-solid border border-white text-slate-100 p-2 rounded-md" onClick={btnLoginClick}> Entrar</button>
+                </div>
+                )
+          }
         </div>
       </header>
       </div>
