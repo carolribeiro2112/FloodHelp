fe42034c161374334dc4a7f39863b4a54b442910
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2024-06-09 19:31:33 -0300

    Feat: Listing basic implementation

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,33 @@
 'use client'
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { getAllOpenRequests } from "./services/Web3Service";
+import Request from "./components/Request";
 
 export default function Home() {
+  const [requests, setRequests] = useState([]);
+
+  useEffect(()=> {
+    loadRequests(0)
+  },[])
+  
+  async function loadRequests(lastId) {
+    try {
+      const result = await getAllOpenRequests(lastId);
+      console.log(result)
+      if(lastId === 0) {
+        setRequests(result)
+      } else {
+        requests.push(...result);
+        setRequests(requests)
+      }
+    } catch(err) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+
   return (
     <>
       <Header/>
@@ -11,7 +36,11 @@ export default function Home() {
           <p className="text-slate-900">Ajude as vítimas de enchentes e demais desastres naturais do Brasil</p>
         </div>
         <div>
-          container lista
+        {
+          requests && requests.length
+            ? requests.map(rq => <Request key={rq.id} data={rq} />)
+            : <>Conect sua carteira MetaMask no botão "Entrar" para ajudar ou pedir ajuda.</>
+        }
         </div>
       </div>
       <Footer/>
--- src/app/services/Web3Service.js
+++ src/app/services/Web3Service.js
@@ -1,4 +1,7 @@
 import Web3 from "web3"
+import ABI from "./ABI.json"
+
+const CONTRACT_ADDRESS = "0x1da21CE916C0B10b25c83d41942C62013c353717";
 
 export async function doLogin() {
   if(!window.ethereum) throw new Error("Sem metamask instalada!");
@@ -9,4 +12,15 @@ export async function doLogin() {
 
   localStorage.setItem("wallet", accounts[0].toLowerCase());
   return accounts[0];
+}
+
+export async function getAllOpenRequests(lastId = 0) {
+  if(!window.ethereum) throw new Error("Sem metamask instalada!");
+
+  const from =localStorage.getItem("wallet");
+  const web3 = new Web3(window.ethereum);
+
+  const contract = new web3.eth.Contract(ABI,CONTRACT_ADDRESS, {from});
+  const requests = await contract.methods.getAllOpenRequests(lastId+1, 10).call();
+  return requests.filter(rq => rq.title !== "")
 }
