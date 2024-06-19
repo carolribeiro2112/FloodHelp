317680046e88d7861f4b0309e865d74d59e6d13e
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2024-06-19 19:28:05 -0300

    Feat: Start styles refactoring - create/page.js

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { openRequest } from "../services/Web3Service";
 
 export default function Home() {
 
@@ -31,45 +32,40 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <div className="container">
-        <div className="ps-5">
-          <div className="row my-3">
+        <div className="p-5">
+          <div className="m-3">
             <p className="text-slate-900">Preencha todos os campos abaixo para nos dizer o que precisa</p>
           </div>
-          <div className="col-6">
+          <div className="m-3">
+            <div className="mb-3 flex flex-col">
+              <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">Resumo do que precisa:</label>
+              <input type="text" id="title" className="block w-1/2 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" maxLength={150} value={request.title} onChange={onInputChange}/>
+            </div>
+          
             <div className="form-floating mb-3">
-              <input type="text" id="title" className="form-control" maxLength={150} value={request.title} onChange={onInputChange}/>
-              <label htmlFor="title">Resumo do que precisa:</label>
+              <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">Descreva em detalhes o que precisa e onde você está (para entregas presenciais):</label>
+              <textarea id="description" className="block w-1/2 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" style={{ height: 100}} value={request.description} onChange={onInputChange}></textarea>
+            </div>
+         
+            <div className="form-floating mb-3">
+              <label htmlFor="contact" className="block text-sm font-medium leading-6 text-gray-900">Contato (telefone ou e-mail):</label>
+              <input type="text" id="contact" className="block w-1/2 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" maxLength={150} value={request.contact} onChange={onInputChange}/>
+            </div>
+          
+            <div className="form-floating mb-3">
+              <label htmlFor="goal" className="block text-sm font-medium leading-6 text-gray-900">Meta em BNB (deixe em branco caso não deseje receber doação em cripto):</label>
+              <input type="number" id="goal" placeholder="0.00" className="block w-1/2 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={request.goal} onChange={onInputChange} />
             </div>
           </div>
-          <div className="col-6">
-            <div className="form-floating mb-3">
-              <textarea id="description" className="form-control" style={{ height: 100}} value={request.description} onChange={onInputChange}></textarea>
-              <label htmlFor="description">Descreva em detalhes o que precisa e onde você está (para entregas presenciais):</label>
+          <div className="flex items-center">
+            <div className="mb-3">
+              <a href="/" className="w-1/3 p-3 outline-1 outline-slate-900">Voltar</a>
             </div>
-          </div>
-          <div className="col-6">
-            <div className="form-floating mb-3">
-              <input type="text" id="contact" className="form-control" maxLength={150} value={request.contact} onChange={onInputChange}/>
-              <label htmlFor="contact">Contato (telefone ou e-mail):</label>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="form-floating mb-3">
-              <input type="number" id="goal" className="form-control" value={request.goal} onChange={onInputChange} />
-              <label htmlFor="goal">Meta em BNB (deixe em branco caso não deseje receber doação em cripto):</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-1 mb-3">
-              <a href="/" className="btn btn-outline-dark col-12 p-3">Voltar</a>
-            </div>
-            <div className="col-5 mb-3 p-0">
-              <button type="button" className="btn btn-dark col-12 p-3" onClick={btnSaveClick}>Enviar Pedido</button>
+            <div className="mb-3 p-0">
+              <button type="button" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={btnSaveClick}>Enviar Pedido</button>
             </div>
           </div>
         </div>
-      </div>
       <Footer/>
     </>
   );
